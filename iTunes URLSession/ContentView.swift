64a05d5fc1b2c2badef046f0a54ebd98ea2d20a9
Abdul R. Arraisi <itsarraisi@gmail.com>
2020-12-26 19:27:10 +0700

    Initial Commit

---
+++ iTunes URLSession/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iTunes URLSession
+//
+//  Created by Abdul R. Arraisi on 26/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iTunes URLSession/iTunes_URLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunes_URLSessionApp.swift
+//  iTunes URLSession
+//
+//  Created by Abdul R. Arraisi on 26/12/20.
+//
+
+import SwiftUI
+
+@main
+struct iTunes_URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
